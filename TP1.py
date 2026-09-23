905a4bf7e72ffe4318abcef92c8ae754b05c46c9
Elhadjor35 <271782558+Elhadjor35@users.noreply.github.com>
2026-09-23 16:10:19 +0200

    Create TP1.py

---
+++ TP1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 15:13:06 2026
+
+@author: elhadj
+"""
+
+import numpy as np
+
+U = np.array(
+    [10.24, 10.79, 11.34, 12.29, 13.33, 14.43, 15.22, 16.24, 17.22, 18.23]
+)
+I = np.array([5.29, 5.45, 5.60, 5.86, 6.12, 6.40, 6.61, 6.82, 7.05, 7.27])
+
+T_0 = (
+    np.array(
+        [
+            1724.9,
+            1761.6,
+            1794.8,
+            1848.8,
+            1906.1,
+            1958.9,
+            2001.7,
+            2042.3,
+            2086.4,
+            2132.6,
+        ]
+    )
+    + 273.15
+)
+
+R = U / I
+
+R_amb = 0.169
+
+rho_amb = 5.65e-8
+
+alpha = R_amb / rho_amb
+
+
+rho_T = R / alpha
+
+T = (rho_T + 7.1e-8) / (0.0320e-8)
+
+h = 6.626e-34
+c = 2.99e8
+lbd = 0.85e-6
+k = 1.38e-23
+c_1 = 8 * np.pi * h * c / (lbd**5)
+c_2 = h * c / (k)
+
+e_lbd_T = c_1 * np.exp(-c_2 / (lbd * T))
+e_lbd_T_0 = c_1 * np.exp(-c_2 / (lbd * T_0))
+epsilon_lbd = e_lbd_T_0 / e_lbd_T
+
+epsilon_moy = np.mean(epsilon_lbd)
+
+# import matplotlib.py
